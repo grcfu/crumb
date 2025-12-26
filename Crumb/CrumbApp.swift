87bff237b03d75076c34df38bc32b16f55dc7e9e
grcfu <gracefu@wustl.edu>
2025-12-25 19:23:16 -0600

    Initial Commit

---
+++ Crumb/CrumbApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrumbApp.swift
+//  Crumb
+//
+//  Created by Grace Fu on 12/25/25.
+//
+
+import SwiftUI
+
+@main
+struct CrumbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
